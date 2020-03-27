074ed237ddb8159241e281ec325b17c9379bc758
Rafael Costa <rafael.costab@hotmail.com>
2020-03-27 00:57:33 -0300

    creating the project

---
+++ src/main/java/com/bionexo/ubs/BioUbsApiApplication.java
@@ -0,0 +1,13 @@
+package com.bionexo.ubs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BioUbsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BioUbsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bionexo/ubs/BioUbsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bionexo.ubs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BioUbsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
