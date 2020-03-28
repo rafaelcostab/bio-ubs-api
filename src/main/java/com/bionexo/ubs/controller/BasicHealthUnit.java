a05b72154968478fe64d3dd0c488a024db3a5194
Rafael Costa <rafael.costab@hotmail.com>
2020-03-28 12:11:06 -0300

    creating controller

---
+++ src/main/java/com/bionexo/ubs/controller/BasicHealthUnit.java
@@ -0,0 +1,18 @@
+package com.bionexo.ubs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("api/v1/")
+public class BasicHealthUnit {
+	
+	@GetMapping("/find_ubs")
+	@ResponseBody
+	public String FindBasicHealthUnit() {
+		return "test";
+	}
+
+}
